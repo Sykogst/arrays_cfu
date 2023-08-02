d700f8559002ac76afc4b32c04de4928ae2d7279
Sam Tran <samuelttran@gmail.com>
2023-08-02 10:07:08 -0600

    Print first and last elements in array

--- arrays.rb
+++ arrays.rb
@@ -4,6 +4,7 @@ ages = [32, 31, 26, 31]
 savings = [123.21, 12.45, 21.67, 52.98]
 likes_apples = [true, false, true, true]
 
+
 # call array methods: pop, push, shift, unshift
 # pop(), removes elements from end of array
 friends.pop(2) # removes last two elements
@@ -20,3 +21,15 @@ p savings       # should print: [12.45, 21.67, 52.98]
 # unshift(), adds elements to beginning of array
 likes_apples.unshift(true) # adds this element to beginning of array
 p likes_apples  # should print: [true, true, false, true, true]
+
+
+# index positions
+# new array to work on
+animals = ['lizards', 'snakes', 'dogs', 'fishes', 'snails']
+#accessing first element in the array
+# indexing starts with 0, so first element is 0
+puts animals[0]    # should print: lizards
+
+# acceessing last element in the array and dont know how many elements exist
+# the element of last index is total length of array minus 1
+puts animals[animals.length - 1]  # should print: snails
