980447d42cba532f2b07fc446e7d9e7635374a84
Sam Tran <samuelttran@gmail.com>
2023-08-02 09:20:54 -0600

    Declare four arrays

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+# declare four array variables: strings, integers, floats, booleans
+friends = ['Sarah', 'Jaqjaq', 'Savi', 'Brady']
+ages = [32, 31, 26, 31]
+savings = [123.21, 1312.45, 21.67, 352.98]
+likes_apples = [true, false, true, true]
